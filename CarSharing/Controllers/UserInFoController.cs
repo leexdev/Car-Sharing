44e36e10fe75f9acf3a111dc0c41ebb709e0cef2
lepro83 <72549881+lepro83@users.noreply.github.com>
2023-06-28 00:07:55 +0700

    feat: update admin

---
+++ CarSharing/Controllers/UserInFoController.cs
@@ -0,0 +1,39 @@
+﻿using CarSharing.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarSharing.Controllers
+{
+    public class UserInFoController : Controller
+    {
+        private readonly VehicleService vehicleService;
+
+        public UserInFoController()
+        {
+            vehicleService = new VehicleService();
+        }
+        public ActionResult TripList()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+
+        public ActionResult TripDetail()
+        {
+            return View();
+        }
+    }
+}
